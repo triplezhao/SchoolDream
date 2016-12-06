2e1f3883a605515a35929c34938647caf2cdeab1
triple.zhao@gmail.com <triple.zhao@gmail.com>
2016-12-06 14:24:15 +0800

    wode 初步布局

--- pages/wode/wode.js
+++ pages/wode/wode.js
@@ -1,9 +1,34 @@
+const AV = require('../../utils/leancloud-storage');
+const Student = require('../../model/Student');
+
 Page({
   data:{
-    text:"Page wode"
+    text:"Page wode",
+    student:null,
+    loadingHidden:false
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+   var that = this;
+     getApp().checkLoginStatus(function (code,data){
+       if(code==1){
+          that.setData({
+          // 拼接数组
+          loadingHidden: true,
+          text:'sadfasdf',
+          student:data
+        })
+       }else{
+         that.setData({
+          // 拼接数组
+          loadingHidden: true,
+          student:data
+        })
+       }
+        
+    });
+
+
   },
   onReady:function(){
     // 页面渲染完成
